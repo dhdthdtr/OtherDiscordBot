cf56e74c49622d88655fb9156c8a55ddadb3c34d
dhdthdtr <78539844+dhdthdtr@users.noreply.github.com>
2021-06-12 12:15:24 +0700

    main file

--- BotLz/src/main/java/BotLz.java
+++ BotLz/src/main/java/BotLz.java
@@ -5,7 +5,7 @@ import net.dv8tion.jda.api.hooks.ListenerAdapter;
 
 public class BotLz {
     public static void main(String[] args) throws Exception {
-        JDA jda = JDABuilder.createDefault("ODM4NjgxMTk3NTUwODk1MTY0.YI-ouw._2PbNqDcK817lhU3CThO7omDmIQ").build();
+        JDA jda = JDABuilder.createDefault(/* Token */).build();
 
         ListenerAdapter[] listenerAdapters = new ListenerAdapter[]{new command(), new TKB(), new avatar(), new VoiceChannelBot(),new LinkMeet(), new Privacy(), new Help(), new meme()};
         jda.addEventListener(listenerAdapters);
